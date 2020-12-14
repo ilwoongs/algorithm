88c2d647a7bba0def8427dc66982718a7b09aa99
ilwoongs <59323208+ilwoongs@users.noreply.github.com>
2020-12-14 10:36:29 -0800

    Create odd_even.cpp

---
+++ odd_even.cpp
@@ -0,0 +1,16 @@
+//Determine if the input number is odd or even
+
+#include <iostream>
+
+using namespace std;
+int main() {
+	int number;
+	cin >> number;
+
+	if(number%2==0)
+		cout<<"even";
+	else
+		cout<<"odd";
+	
+	return 0;
+}
