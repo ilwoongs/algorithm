68f63180f50996a61bb931d3c3b212f53f249b05
ilwoongs <59323208+ilwoongs@users.noreply.github.com>
2020-12-15 12:13:50 -0800

    Update two_sum.cpp

--- two_sum.cpp
+++ two_sum.cpp
@@ -1,33 +1,22 @@
 //Given an array of integers, return the indices of the two numbers that add up to a given target
 
-#include <iostream>
-using namespace std;
-
-//Searching a pair algorithm
-int search(int *nums, int target,int len){
-
-    for(int i=0;i<len;i++){
-        for(int j=i+1;j<len;j++){
-            if(nums[i] + nums[j] == target){
-                cout<<i<<" "<<j<<endl;
-                return 1;
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> result = {0,0};
+        int leng = sizeof(nums)/6;
+        
+        for(int i=0;i<leng;i++){
+            for(int j=i+1;j<leng;j++){
+                if (nums[i]+nums[j] == target){
+                    result[0] = i;
+                    result[1] = j;
+                }
             }
         }
+        
+        return result;
     }
-
-    return 0;
-}
-
-int main(){
-    int nums[5] = {1,3,7,9,2};
-    int target = 11;
-    int len = sizeof(nums)/4;
-
-    int result = search(nums, target, len);
-    
-    if(result == 0)
-        cout<<"The pair not found"<<endl;
-
-    return 0;
-}
-
+        
+  
+};
